962fb97b7ae73c8a58511dd19ba020ca10a27629
Harshita Tyagi <100477854+Harshitatyagi@users.noreply.github.com>
2024-06-27 18:45:57 +0530

    Update ProjectCard.js

--- src/Components/ProjectCard.js
+++ src/Components/ProjectCard.js
@@ -5,21 +5,36 @@ import { FaGithubAlt } from 'react-icons/fa'
 
 const ProjectCard = ({projectName,projectImage}) => {
   const desc = {
-    MemorifyDesc : "A landing page website for Cats developed using HTML, CSS and Bootstrap. It is completely responsive",
+    DoubtSmasherDesc:
+    DoubtSmasherGithub:
+    DoubtSmasherWebsite:
+  
+    MemorifyDesc : "",
      MemorifyGithub : "https://github.com/Harshitatyagi/my-memories-client",
      MemorifyWebsite : "https://harshitamemories.netlify.app/",
 
+    dijkstraAlgorithmDesc:" ",
+    dijkstraAlgorithmWebsite:"https://github.com/Harshitatyagi/dijkstraAlgorithm",
+     dijkstraAlgorithmGithub:"https://dijkstra-algorithm11.netlify.app/",
+    
     PortfolioDesc : "Dynamic personal portfolio showcasing skills and projects, built with React.js for a captivating and interactive web experience.",
-    PortfolioGithub : "https://github.com/reach-anu/Portfolio",
-    PortfolioWebsite : "https://anushaka.netlify.app/",
-
-    reacttodoappDesc : "A simple clone of Google keep made using React.js and react-props", 
+    PortfolioGithub : "https://github.com/Harshitatyagi/Harshita-Portfolio",
+    PortfolioWebsite : "https://harshita11portfolio.netlify.app/",
+    
+    reacttodoappDesc : "A simple TodoApp to keep along your daily schedule on track.", 
    reacttodoappWebsite : "https://harshita-todo.netlify.app/",
     reacttodoappGithub :  "https://github.com/Harshitatyagi/react-todo-app", 
 
-    dijkstraAlgorithmDesc:"A comprehensive pet-friendly website which serves as a one-stop destination for all the pet. A group project made in a team of four. ",
-    dijkstraAlgorithmWebsite:"https://github.com/Harshitatyagi/dijkstraAlgorithm",
-     dijkstraAlgorithmGithub:"https://dijkstra-algorithm11.netlify.app/",
+    
+     blogappDesc : "A Blog website for technical articles developed using react", 
+   blogappWebsite : "https://blogreact11.netlify.app/",
+    blogappGithub :  "https://github.com/Harshitatyagi/blog_react_app", 
+
+      tourDesc : "A simple clone of Google keep made using React.js and react-props", 
+   tourWebsite : "https://tourism11react.netlify.app",
+    tourGithub :  "https://github.com/Harshitatyagi/Tourism_cards", 
+
+    
   }
 
 
